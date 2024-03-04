240d7fb9bf48ad008dea2c68a1512440eb7eef28
Jonathan Leprini <leprini.jonathan@protonmail.com>
2024-03-04 16:31:16 +0100

    Structure HTML messagerie

--- views/templates/messaging.php
+++ views/templates/messaging.php
@@ -1,4 +1,49 @@
 <?php
 ?>
 
-<h1>Page de messagerie</h1>
+<section>
+    <div class="overall-threads">
+        <h2>Messagerie</h2>
+
+        <div class="thread-discussion">
+            <!--  ZONE BOUCLE PHP  -->
+            <div class="discussion-link">
+
+            </div>
+            <!--  FIN BOUCLE PHP  -->
+        </div>
+    </div>
+
+    <div class="view-discussion">
+        <div class="contact-discussion">
+            <img src="" alt="">
+            <div class="contact-username"></div>
+        </div>
+
+        <div class="discussion">
+            <!--  ZONE BOUCLE PHP  -->
+            <div class="message">
+                <div class="message-info">
+                    <img src="" alt="">
+                    <div class="message-date-time">
+
+                    </div>
+                </div>
+                <div class="message-content">
+
+                </div>
+            </div>
+            <!--  FIN BOUCLE PHP  -->
+        </div>
+
+        <div class="add-message-bloc">
+            <form action="">
+                <label for="add-message"></label>
+                <textarea id="add-message" name="message">
+
+                </textarea>
+                <input type="submit" value="Envoyer">
+            </form>
+        </div>
+    </div>
+</section>
